76c999b77a1f01a375df82a7275c5222e57705ad
Louis Tur <louis.tur@gmail.com>
2016-05-26 19:45:18 -0400

    Step 2: moving out colors into separate file, adding in views and setting up hierarchy

---
+++ FollowButton/ColorPalette.swift
@@ -0,0 +1,16 @@
+//
+//  ColorPalette.swift
+//  FollowButton
+//
+//  Created by Louis Tur on 5/26/16.
+//  Copyright © 2016 cat.thoughts. All rights reserved.
+//
+
+import UIKit
+
+internal struct ConceptColors {
+  static let LightBlue: UIColor = UIColor(colorLiteralRed: 130.0/255.0, green: 222.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+  static let MediumBlue: UIColor = UIColor(colorLiteralRed: 113.0/255.0, green: 156.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+  static let OffWhite: UIColor = UIColor(colorLiteralRed: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0)
+  static let DarkText: UIColor = UIColor(colorLiteralRed: 100.0/255.0, green: 100.0/255.0, blue: 150.0/255.0, alpha: 1.0)
+}
--- FollowButton/ProfileViewController.swift
+++ FollowButton/ProfileViewController.swift
@@ -10,31 +10,60 @@ import UIKit
 import SnapKit
 
 /**
-  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  
-    Design Idea Found @ http://www.ios.uplabs.com/posts/profile-page-interaction-and-animation
-    Designer: Malik, @iOfficialBlack (http://www.ios.uplabs.com/iOfficialBlack)
+ >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  
-  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-*/
+ Design Idea Found @ http://www.ios.uplabs.com/posts/profile-page-interaction-and-animation
+ Designer: Malik, @iOfficialBlack (http://www.ios.uplabs.com/iOfficialBlack)
+ 
+ >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ */
 
-internal struct ConceptColors {
-  static let LightBlue: UIColor = UIColor(colorLiteralRed: 130.0/255.0, green: 222.0/255.0, blue: 255.0/255.0, alpha: 1.0)
-  static let MediumBlue: UIColor = UIColor(colorLiteralRed: 113.0/255.0, green: 156.0/255.0, blue: 255.0/255.0, alpha: 1.0)
-  static let OffWhite: UIColor = UIColor(colorLiteralRed: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0)
-  static let DarkText: UIColor = UIColor(colorLiteralRed: 100.0/255.0, green: 100.0/255.0, blue: 150.0/255.0, alpha: 1.0)
-}
 
 class ProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-
-    }
-
+  
+  
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.setupViewHierarchy()
+    self.configureConstraints()
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+  
+  
+  // MARK: - Layout
+  internal func configureConstraints() {
+    
+  }
+  
+  internal func setupViewHierarchy() {
+    self.view.addSubview(profileBackgroundView)
+    self.profileBackgroundView.addSubview(self.profileTopSectionView)
+    self.profileBackgroundView.addSubview(self.profileBackgroundView)
+  }
+  
+  
+  // MARK: - Lazy Instances
+  lazy var profileBackgroundView: UIView = {
+    let view: UIView = UIView()
+    view.layer.cornerRadius = 12.0
+    view.clipsToBounds = true
+    return view
+  }()
+  
+  lazy var profileTopSectionView: UIView = {
+    let view: UIView = UIView()
+    return view
+  }()
+  
+  lazy var profileBottomSectionView: UIView = {
+    let view: UIView = UIView()
+    view.backgroundColor = ConceptColors.OffWhite
+    return view
+  }()
+  
 }
