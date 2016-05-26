24312dcf40da81efd530b1f0c437286c24ccda2f
Louis Tur <louis.tur@gmail.com>
2016-05-26 19:38:23 -0400

    Step 1: Setup & Acknowledgements

---
+++ FollowButton/ProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileViewController.swift
+//  FollowButton
+//
+//  Created by Louis Tur on 5/26/16.
+//  Copyright © 2016 cat.thoughts. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+/**
+  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  
+    Design Idea Found @ http://www.ios.uplabs.com/posts/profile-page-interaction-and-animation
+    Designer: Malik, @iOfficialBlack (http://www.ios.uplabs.com/iOfficialBlack)
+ 
+  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*/
+
+internal struct ConceptColors {
+  static let LightBlue: UIColor = UIColor(colorLiteralRed: 130.0/255.0, green: 222.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+  static let MediumBlue: UIColor = UIColor(colorLiteralRed: 113.0/255.0, green: 156.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+  static let OffWhite: UIColor = UIColor(colorLiteralRed: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0)
+  static let DarkText: UIColor = UIColor(colorLiteralRed: 100.0/255.0, green: 100.0/255.0, blue: 150.0/255.0, alpha: 1.0)
+}
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+}
